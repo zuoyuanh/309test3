926b9fe87f9477bdbeed7c4d8e87d7761b49a686
Zuoyuan Huang <david@Davids-MacBook-Pro.local>
2018-02-26 14:42:23 -0800

    first commit

---
+++ src/main/java/logic/WeightConversion.java
@@ -0,0 +1,15 @@
+package logic;
+
+
+public class WeightConversion {
+
+	public double KilosToPounds (double kilos){
+		
+		return (kilos / 0.454);
+	}
+	
+public double PoundsToKilos (double pounds){
+		
+		return (pounds * 0.454);
+	}
+}
